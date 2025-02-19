5db4835896ee9619b2e02fd5cdeca1b4c56ab1da
David Christian <136609271+Myaneeeee@users.noreply.github.com>
2025-02-19 13:40:18 +0700

    landing page selesai

--- src/frontend/src/pages/landing/landing-page.tsx
+++ src/frontend/src/pages/landing/landing-page.tsx
@@ -1,11 +1,17 @@
+import Footer from '../../components/partials/footer';
 import LandingMain from '../../components/partials/landing-main';
 import Navbar from '../../components/partials/navbar';
 
 export default function LandingPage() {
   return (
-    <div className="w-screen h-screen bg-background">
-      <Navbar></Navbar>
-      <LandingMain></LandingMain>
+    <div className="w-screen min-h-screen flex flex-col bg-background">
+      <Navbar />
+
+      <div className="flex-1">
+        <LandingMain />
+      </div>
+
+      <Footer />
     </div>
   );
 }
